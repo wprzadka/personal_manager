f7d690dcba6bf61795633c5b28b11a637046eee2
Viking <wiktor.przadka.1@gmail.com>
2020-11-06 23:17:21 +0100

    refactor columns layout

--- src/manager/ui/DesktopUserInterface.java
+++ src/manager/ui/DesktopUserInterface.java
@@ -13,8 +13,8 @@ import java.util.List;
 
 public class DesktopUserInterface implements UserInterface {
 
-    int width;
-    int height;
+    private int width;
+    private int height;
 
     public DesktopUserInterface(int width, int height){
         this.width = width;
@@ -31,25 +31,21 @@ public class DesktopUserInterface implements UserInterface {
         for(var name : colNames){
 
             var column = new VBox();
-//            column.setStyle("-fx-background-color: #AABBCC;");
 
             column.setBackground(
                     new Background(
                             new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY)
                     )
             );
-
             var title = new Text(name);
             title.setFont(Font.font(null, FontWeight.BOLD, 26));
-            title.setCaretBias(true);
 
             column.getChildren().add(title);
-            column.setPrefWidth(width / 3);
+            column.setPrefWidth(width);
             column.setAlignment(Pos.TOP_CENTER);
-            column.setStyle("-fx-border-style: dotted;");
+            column.setStyle("-fx-border-style: solid;");
             columns.add(column);
         }
-
         layout.getChildren().addAll(columns);
 
         return layout;
