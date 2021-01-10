1d1300e863c1aa17c85915a4c74e8f8cc42c0617
viking <wiktor.przadka.1@gmail.com>
2021-01-10 14:41:46 +0100

    remove unnecessary variables

--- src/manager/ManagerApp.java
+++ src/manager/ManagerApp.java
@@ -4,31 +4,24 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 import manager.configuration.Configuration;
-import manager.ui.UserInterface;
-import manager.database.DbConnection;
-import manager.controller.MainEventsController;
 
 
 public class ManagerApp extends Application {
-
-    private final DbConnection dbConn = Configuration.getInstance().getDbConnection();
-    private final UserInterface ui = Configuration.getInstance().getUserInterface();
-    private final MainEventsController eventController = Configuration.getInstance().getMainEventsController();
 
     @Override
     public void start(Stage stage) {
         Configuration.getInstance().getContentContainer().loadComponents();
 
         stage.setTitle("Task Manager");
-        stage.setScene(ui.getScene());
-        eventController.refresh();
+        stage.setScene(Configuration.getInstance().getUserInterface().getScene());
+        Configuration.getInstance().getMainEventsController().refresh();
         stage.show();
     }
 
     @Override
     public void stop(){
 
-        dbConn.close();
+        Configuration.getInstance().getDbConnection().close();
     }
 
     public static void main(String[] args){
